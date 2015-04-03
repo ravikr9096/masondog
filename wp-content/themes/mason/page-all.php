c95bea5e3e34db48fbf77eafdccfeef4eaef40a9
Ravi <ravikr9096@gmail.com>
2015-04-03 21:31:09 +0530

    all the filter on collection is working now

--- wp-content/themes/mason/page-all.php
+++ wp-content/themes/mason/page-all.php
@@ -11,59 +11,52 @@ get_header('inner');?>
 				<ul class="control-btns">
 					<li class="filter" data-filter="all"><a href="<?php echo home_url();?>/all">All</a></li>
 					<li class="filter" data-filter=".category-1"><a href="<?php echo home_url();?>/new-arrival">NEW ARRIVALS</a></li>
-					<li class="filter" data-filter=".category-2">TANKS</li>
-					<li class="filter" data-filter=".category-3">T-SHIRTS</li>
+					<?php
+					$args = array(
+						'number'     => $number,
+						'orderby'    => $orderby,
+						'order'      => $order,
+						'hide_empty' => $hide_empty,
+						'include'    => $ids
+					);
+					$product_categories = get_terms( 'product_cat', $args );
+					$count = count($product_categories);
+					if ( $count > 0 ){
+						foreach ( $product_categories as $product_category ) {
+							echo '<li class="filter"><a href="'. home_url().'/collections?category='.$product_category->slug.'" title="'.$product_category->name.'">'.$product_category->name.'
+					</a></li>';
+						}
+					}
+					?>
+					
 				</ul>
 				<span>Styles</span>
 			</div>
 			
-			<ul id="Container" class="container">
-			
+			<ul class="fetur-lst all-lst">
 			<?php
-			$args = array(
-				'number'     => $number,
-				'orderby'    => $orderby,
-				'order'      => $order,
-				'hide_empty' => $hide_empty,
-				'include'    => $ids
-			);
-			$product_categories = get_terms( 'product_cat', $args );
-			$count = count($product_categories);
-			if ( $count > 0 ){
-				foreach ( $product_categories as $product_category ) {
-					if($product_category->slug == 'featured-products'){
-						continue;
-					}
-					//echo '<li><div class="prdt-nm"><a href="' . get_term_link( $product_category ) . '">' . $product_category->name .'</a>';
-					echo '<li class="mix category-1">
-							<div class="prdt-nm">
-								<a >'. $product_category->name .'</a>
-							</div><div class="prdt-img">';
-			?>
-			<?php
-			$args = array( 'post_type' => 'product', 'posts_per_page' => 20, 'product_cat' => $product_category->slug, 'orderby' => 'rand' );
-			$loop = new WP_Query( $args );
-			while ( $loop->have_posts() ) : $loop->the_post(); global $product;
-			//print_r($product);
-			?>
-								
-				
-					<a href="<?php echo $product->post->guid?>" class="prd-img">
-					<?php if (has_post_thumbnail( $loop->post->ID )) echo get_the_post_thumbnail($loop->post->ID, 'shop_catalog'); else echo '<img src="'.woocommerce_placeholder_img_src().'" alt="Placeholder" width="300px" height="300px" />'; ?> <span class="prd-inf"><?php the_title(); ?><br><?php echo $product->get_price_html(); ?></span>
-					
-					
-					</a>
-					
-				
-				 <?php //woocommerce_template_loop_add_to_cart( $loop->post, $product ); ?>
-				
-				<?php endwhile; ?>
-				<?php echo '</div><br/></li>';?>
-				<?php };?>
-				</div>
-				</li>
-				<?php };?>
-					</ul>
+				$args = array(  
+								'post_type' => 'product',  
+								'meta_key' => '_featured',  
+								'meta_value' => 'no',  
+								'posts_per_page' => 10  
+							);  
+				$loop = new WP_Query( $args );
+				while ( $loop->have_posts() ) : $loop->the_post(); global $product; ?>
+
+						<li >    
+
+							<a href="<?php echo get_permalink( $loop->post->ID ) ?>" title="<?php echo esc_attr($loop->post->post_title ? $loop->post->post_title : $loop->post->ID); ?>">
+
+								<?php if (has_post_thumbnail( $loop->post->ID )) echo get_the_post_thumbnail($loop->post->ID, 'shop_catalog'); else echo '<img src="'.woocommerce_placeholder_img_src().'" alt="Placeholder" />'; ?>                   
+							<h2><?php echo esc_attr($loop->post->post_title ? $loop->post->post_title : $loop->post->ID); ?></h2>
+							</a>
+
+						</li>
+
+			<?php endwhile; ?>
+			<?php wp_reset_query(); ?>
+		</ul>
 				<!------------------------------------------------->
 
 				<!------------------------------------------------->
